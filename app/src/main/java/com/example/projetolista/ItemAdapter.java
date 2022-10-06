f5c8a5a605d3faefffab0163bbf32b55c5e4be46
WESLEY DE OLIVEIRA DE PAULA <7503397@fmu.interno>
2022-10-05 21:26:51 -0300

    adapter

--- app/src/main/java/com/example/projetolista/ItemAdapter.java
+++ app/src/main/java/com/example/projetolista/ItemAdapter.java
@@ -2,10 +2,13 @@ package com.example.projetolista;
 
 import android.content.Context;
 import android.database.DataSetObserver;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,6 +29,13 @@ public class ItemAdapter extends ArrayAdapter<Item> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(this.context);
+        convertView = inflater.inflate(R.layout.item, parent, false);
+        Item i = this.elementos.get(position);
+        TextView txtTitulo = (TextView) convertView.findViewById(R.id.txtTitulo);
+        TextView txtSubtitulo = (TextView) convertView.findViewById(R.id.txtSubtitulo);
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView);
+
         return super.getView(position, convertView, parent);
     }
 }
